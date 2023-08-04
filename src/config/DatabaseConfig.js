5de55960a78b08cfa2eac289b1cfbd20687ade63
AshleyWrightA <AshleyWrightAppDev@outlook.com>
2023-08-03 19:08:44 -0600

    Database config changes

--- src/config/DatabaseConfig.js
+++ src/config/DatabaseConfig.js
@@ -1,21 +1,7 @@
 import { MongoClient } from "mongodb";
-import AWS from "aws-sdk";
 
-async function getMongoConnectionString() {
-  const secretManager = new AWS.SecretsManager();
-
-  try {
-    const data = await secretManager.getSecretValue({ SecretId: secretName }).promise();
-    const secret = JSON.parse(data.SecretString);
-    return secret.mongoConnectionString;
-  } catch (err) {
-    console.error("Failed to retreive connection string");
-    throw err;
-  }
-}
-
-export default async function getClient() {
-  const uri = await getMongoConnectionString();
+export default function getClient() {
+  const uri = process.env.MONGO_CONNECTION_STRING;
   const client = new MongoClient(uri);
   return client;
 }
